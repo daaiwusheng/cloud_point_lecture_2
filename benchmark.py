76da9885141c43e10f47953eb098b413aaf6c013
YuWang(Steven) <daaiwusheng003@gmail.com>
2022-02-20 19:28:59 +0800

    change the code to load only one dataset file

--- benchmark.py
+++ benchmark.py
@@ -33,40 +33,37 @@ def main():
     k = 8
     radius = 1
 
-    root_dir = '/Users/renqian/cloud_lesson/kitti'  # 数据集路径
-    cat = os.listdir(root_dir)
-    iteration_num = len(cat)
-
+    root_dir = './000000.bin'  # 数据集路径
     print("octree --------------")
     construction_time_sum = 0
     knn_time_sum = 0
     radius_time_sum = 0
     brute_time_sum = 0
-    for i in range(iteration_num):
-        filename = os.path.join(root_dir, cat[i])
-        db_np = read_velodyne_bin(filename)
 
-        begin_t = time.time()
-        root = octree.octree_construction(db_np, leaf_size, min_extent)
-        construction_time_sum += time.time() - begin_t
+    filename = root_dir
+    db_np = read_velodyne_bin(filename)
 
-        query = db_np[0, :]
+    begin_t = time.time()
+    root = octree.octree_construction(db_np, leaf_size, min_extent)
+    construction_time_sum += time.time() - begin_t
 
-        begin_t = time.time()
-        result_set = KNNResultSet(capacity=k)
-        octree.octree_knn_search(root, db_np, result_set, query)
-        knn_time_sum += time.time() - begin_t
+    query = db_np[0, :]
 
-        begin_t = time.time()
-        result_set = RadiusNNResultSet(radius=radius)
-        octree.octree_radius_search_fast(root, db_np, result_set, query)
-        radius_time_sum += time.time() - begin_t
+    begin_t = time.time()
+    result_set = KNNResultSet(capacity=k)
+    octree.octree_knn_search(root, db_np, result_set, query)
+    knn_time_sum += time.time() - begin_t
 
-        begin_t = time.time()
-        diff = np.linalg.norm(np.expand_dims(query, 0) - db_np, axis=1)
-        nn_idx = np.argsort(diff)
-        nn_dist = diff[nn_idx]
-        brute_time_sum += time.time() - begin_t
+    begin_t = time.time()
+    result_set = RadiusNNResultSet(radius=radius)
+    octree.octree_radius_search_fast(root, db_np, result_set, query)
+    radius_time_sum += time.time() - begin_t
+
+    begin_t = time.time()
+    diff = np.linalg.norm(np.expand_dims(query, 0) - db_np, axis=1)
+    nn_idx = np.argsort(diff)
+    nn_dist = diff[nn_idx]
+    brute_time_sum += time.time() - begin_t
     print("Octree: build %.3f, knn %.3f, radius %.3f, brute %.3f" % (construction_time_sum * 1000 / iteration_num,
                                                                      knn_time_sum * 1000 / iteration_num,
                                                                      radius_time_sum * 1000 / iteration_num,
@@ -77,31 +74,28 @@ def main():
     knn_time_sum = 0
     radius_time_sum = 0
     brute_time_sum = 0
-    for i in range(iteration_num):
-        filename = os.path.join(root_dir, cat[i])
-        db_np = read_velodyne_bin(filename)
 
-        begin_t = time.time()
-        root = kdtree.kdtree_construction(db_np, leaf_size)
-        construction_time_sum += time.time() - begin_t
+    begin_t = time.time()
+    root = kdtree.kdtree_construction(db_np, leaf_size)
+    construction_time_sum += time.time() - begin_t
 
-        query = db_np[0, :]
+    query = db_np[0, :]
 
-        begin_t = time.time()
-        result_set = KNNResultSet(capacity=k)
-        kdtree.kdtree_knn_search(root, db_np, result_set, query)
-        knn_time_sum += time.time() - begin_t
+    begin_t = time.time()
+    result_set = KNNResultSet(capacity=k)
+    kdtree.kdtree_knn_search(root, db_np, result_set, query)
+    knn_time_sum += time.time() - begin_t
 
-        begin_t = time.time()
-        result_set = RadiusNNResultSet(radius=radius)
-        kdtree.kdtree_radius_search(root, db_np, result_set, query)
-        radius_time_sum += time.time() - begin_t
+    begin_t = time.time()
+    result_set = RadiusNNResultSet(radius=radius)
+    kdtree.kdtree_radius_search(root, db_np, result_set, query)
+    radius_time_sum += time.time() - begin_t
 
-        begin_t = time.time()
-        diff = np.linalg.norm(np.expand_dims(query, 0) - db_np, axis=1)
-        nn_idx = np.argsort(diff)
-        nn_dist = diff[nn_idx]
-        brute_time_sum += time.time() - begin_t
+    begin_t = time.time()
+    diff = np.linalg.norm(np.expand_dims(query, 0) - db_np, axis=1)
+    nn_idx = np.argsort(diff)
+    nn_dist = diff[nn_idx]
+    brute_time_sum += time.time() - begin_t
     print("Kdtree: build %.3f, knn %.3f, radius %.3f, brute %.3f" % (construction_time_sum * 1000 / iteration_num,
                                                                      knn_time_sum * 1000 / iteration_num,
                                                                      radius_time_sum * 1000 / iteration_num,
